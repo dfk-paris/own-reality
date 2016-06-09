c9794957177daf5b8e1adc0cbedfc79a2bab388b
Moritz Schepp <moritz.schepp@gmail.com>
2016-06-09 14:31:42 +0200

    fixes url CORS?

--- config/routes.rb
+++ config/routes.rb
@@ -13,16 +13,16 @@ Rails.application.routes.draw do
   end
 
   defaults :format => "json" do
-    match "api/chronology", :to => "entities#chronology", :via => :post
+    match "api/chronology", :to => "entities#chronology", :via => [:post, :get]
     match "api/papers/:type", :to => "papers#index", :via => :get
     match "api/items/:type/:id", :to => "entities#show", :via => :get
-    match "api/entities/search", :to => "entities#search", :via => :post
-    match "api/entities/lookup", :to => "entities#lookup", :via => :post
-    match "api/entities/:id", :to => "entities#show", :via => :post
+    match "api/entities/search", :to => "entities#search", :via => [:post, :get]
+    match "api/entities/lookup", :to => "entities#lookup", :via => [:post, :get]
+    match "api/entities/:id", :to => "entities#show", :via => [:post, :get]
     match "api/entities/download/:hash.:format", :to => "entities#download", :via => :get
     match "api/translations", :to => "session#translations", :via => :get
     match "api/misc", :to => "misc#index", :via => :get
-    match 'api/people', to: 'people#index', via: :post
+    match 'api/people', to: 'people#index', via: [:post, :get]
   end
 
 end
